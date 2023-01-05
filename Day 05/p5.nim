b881c082987fec5ff2e89021c6e324c5869ef6cb
Luc Secouard <luc.secouard@laposte.net>
2023-01-05 19:52:18 +0100

    Update p5.nim

--- Day 05/p5.nim
+++ Day 05/p5.nim
@@ -1,23 +1,23 @@
-import strutils
+import std/strutils
 
-let data = "data".readFile()
+let data = readFile("p5.data")
 
 proc react(data: string): int =
-    var res = "\0"    # Add a char at beginning to simplify the algorithm.
-    for c in data:
-      if c != res[^1] and c.toLowerAscii == res[^1].toLowerAscii:
-          res.setLen(res.high)  # Pop one char.
-      else:
-        res.add(c)              # Add char to result.
-    result = res.len - 1
+  ## Apply the reactions to the polymer described by the "data" string.
+  var res = "\0"    # Add a char at beginning to simplify the algorithm.
+  for c in data:
+    if c != res[^1] and c.toLowerAscii == res[^1].toLowerAscii:
+      res.setLen(res.high)  # Pop one char.
+    else:
+      res.add(c)            # Add char to result.
+  result = res.len - 1
 
-##########################################
-# Part 1.
 
+### Part 1 ###
 echo "Part 1: ", data.react()
 
-##########################################
-# Part 2.
+
+### Part 2 ###
 
 # Find the unit types.
 var units: set[char]
