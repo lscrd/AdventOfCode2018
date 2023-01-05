73edea3ee1711df1e9c667206e5dbd3189f22310
Luc Secouard <luc.secouard@laposte.net>
2023-01-05 22:43:54 +0100

    Update p9.nim
    Minor changes.

--- Day 09/p9.nim
+++ Day 09/p9.nim
@@ -1,17 +1,20 @@
-import lists
+import std/lists
 
 const
-  PLAYERS = 441
+  Players = 441
+  Rounds = 71_032
 
 var
-  ring = initDoublyLinkedRing[int]()
-  scores: array[PLAYERS, int]
+  ring: DoublyLinkedRing[int]
+  scores: array[Players, int]
 
-proc addMarble(ring: var DoublyLinkedRing[int], marble: int) =
+proc addMarble(ring: var DoublyLinkedRing[int]; marble: int) =
+  ## Add a marble in the ring.
   ring.head = ring.head.next.next
   ring.prepend(marble)
 
 proc removedMarble(ring: var DoublyLinkedRing[int]): int =
+  ## Remove a marble from the ring and return its number.
   var node = ring.head
   for _ in 1..7:
     node = node.prev
@@ -19,7 +22,8 @@ proc removedMarble(ring: var DoublyLinkedRing[int]): int =
   ring.head = node.next
   ring.remove(node)
 
-proc run(ring: var DoublyLinkedRing, scores: var openarray[int], rounds: int) =
+proc run(ring: var DoublyLinkedRing; scores: var openarray[int]; rounds: int) =
+  ## Run the game for the given number of rounds.
 
   # Add first marble.
   ring.append(0)
@@ -28,22 +32,20 @@ proc run(ring: var DoublyLinkedRing, scores: var openarray[int], rounds: int) =
   var player = 0
   for marble in 1..<rounds:
     inc player
-    if player == PLAYERS: player = 0
+    if player == Players: player = 0
     if marble mod 23 != 0:
       ring.addMarble(marble)
     else:
       inc scores[player], marble + ring.removedMarble()
 
 
-################################################
-# Part 1.
+### Part 1 ###
 
-run(ring, scores, 71_032)
+run(ring, scores, Rounds)
 echo "Part 1: ", max(scores)
 
 
-################################################
 # Part 2.
 
-run(ring, scores, 7_103_200)
+run(ring, scores, Rounds * 100)
 echo "Part 2: ", max(scores)
